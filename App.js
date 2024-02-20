114fc65ea9bd1a9cbf9b8f61b62d639c5e94575a
JogailaJan <jogaila.janusas@gmail.com>
2024-02-20 22:28:42 +0200

    Bandziau dirbti ant favorites
    +1h

--- App.js
+++ App.js
@@ -8,20 +8,38 @@ import AreasDescriptionScreen from "./screens/AreasDescription";
 import FavoritesScreen from "./screens/Favorites";
 
 const Stack = createNativeStackNavigator();
+const starEmpty = require("./star-empty.png");
+
 
 export default function App() {
   return (
-
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Questionnaire" component={QuestionnaireScreen} />
-        <Stack.Screen name="Smart Home" component={SmartHomeScreen} />
-        <Stack.Screen name="Systems Description" component={SystemsDescriptionScreen} />
-        <Stack.Screen name="Areas Description" component={AreasDescriptionScreen} />
-        <Stack.Screen name="Favorites" component={FavoritesScreen} />
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ 
+            title: 'Home',
+            headerStyle: { backgroundColor: 'white' },
+            headerTintColor: 'black',
+            headerTitleStyle: { fontWeight: 'bold' },
+          }} 
+        />
+        <Stack.Screen 
+          name="Questionnaire" 
+          component={QuestionnaireScreen} 
+          options={{ 
+            title: 'Questionnaire',
+            headerStyle: { backgroundColor: 'green' },
+            headerTintColor: 'white',
+            headerTitleStyle: { fontWeight: 'bold' },
+          }} 
+        />
+        <Stack.Screen name="Smart Home" component={SmartHomeScreen} options={{ title: 'Smart Home' }} />
+        <Stack.Screen name="Systems Description" component={SystemsDescriptionScreen} options={{ title: 'Systems Description' }} />
+        <Stack.Screen name="Areas Description" component={AreasDescriptionScreen} options={{ title: 'Areas Description' }} />
+        <Stack.Screen name="Favorites" component={FavoritesScreen} options={{ title: 'Favorites' }} />
       </Stack.Navigator>
-    </NavigationContainer>                                                                                 
+    </NavigationContainer>                                                                            
   );
-}                            
-
+}
