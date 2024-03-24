62b402d666d85ebc38987447b9d90b91de3b9c3c
viktoras8 <viktoraseikis@gmail.com>
2024-03-24 11:24:15 +0200

    Rasomi duomenys duomenu bazeje

--- backend/Functionalities/Add.js
+++ backend/Functionalities/Add.js
@@ -1,10 +1,12 @@
 export const functionalitiesToAdd = [
+  /////Apsvietimas
+
   {
     name: "Perjungimas",
     group: "value 1",
     value: "switching",
     longDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "tai procesas, kai elektrinė įranga arba prietaisai yra įjungiami arba išjungiami. Šis procesas gali būti atliekamas rankiniu būdu, naudojant mechaninius arba elektroninius įrenginius, arba automatiškai, pagal tam tikrus nustatytus sąlygų ar veikimo principų. ",
     shortDescription: "Short description 1",
   },
   {
@@ -12,7 +14,7 @@ export const functionalitiesToAdd = [
     group: "value 1",
     value: "dimming",
     longDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "Tai  procesas, kai reguliuojama šviesos šaltinio išspinduliavimo galia, kad būtų pasiektas norimas šviesos lygis. Tai gali būti atliekama naudojant specialius įtaisus arba technologijas, kurios keičia elektros srovės įtampą arba dažnį, taip mažindamos ar didindamos šviesos stiprumą.",
     shortDescription: "Short description 1",
   },
   {
@@ -20,7 +22,250 @@ export const functionalitiesToAdd = [
     group: "value 1",
     value: "colour-temperature",
     longDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "Tai yra matavimas, skirtas apibūdinti šviesos spalvą, kuri yra išreikšta Kelvinų (K) skalėje. Ji apibūdina šviesos šaltinio spektrinį sudėjimą ir yra susijusi su spalvos tonu, kuriame šviesos šaltinis spinduliuoja.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Spalva",
+    group: "value 1",
+    value: "colour",
+    longDescription:
+      "Tai yra svarbi funkcija šviesos apšvietime, nes ji nulemia objekto arba scenos vizualinį įspūdį. Šviesos spalva priklauso nuo šviesos šaltinio spektrinio turinio ir apšvietimo aplinkos.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Seka",
+    group: "value 1",
+    value: "sequences",
+    longDescription:
+      "sviesos apšvietime gali būti suprantama kaip iš anksto nustatytas šviesos šaltinio veiksmų arba šviesos šviesos efektų serijos. Tai gali apimti šviesos šaltinio įjungimą arba išjungimą tam tikru laiku, spalvų perėjimą, šviesos stiprumo pokyčius, judančius šviesos efektus ir kitus šviesos pokyčius. ",
+    shortDescription: "Short description 1",
+  },
+
+  ///////Zaliuzes
+
+  {
+    name: "Langinės",
+    group: "value 2",
+    value: "shutters",
+    longDescription:
+      "Šviesos apšvietime gali būti naudojamos kaip funkcija, leidžianti reguliuoti šviesos srautą arba intensyvumą. Šviesos skliautai yra reguliuojami lanksčios konstrukcijos elementai, kurie gali būti atidaromi arba uždaromi, siekiant kontroliuoti šviesos kiekį, kuris patenka į tam tikrą erdvę arba objektą. ",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Markizės",
+    group: "value 2",
+    value: "awnings",
+    longDescription:
+      "Tai  yra apsauginiai ir dekoratyviniai elementai, kurie dedami virš langų, durų ar terasų, siekiant apsaugoti nuo tiesioginių saulės spindulių arba lietaus.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Vėdinimo sklendės",
+    group: "value 2",
+    value: "ventilation flaps",
+    longDescription:
+      "Tai yra elementai, kurie leidžia reguliuoti oro srautą tarp vidaus ir išorės.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Stoglangiai",
+    group: "value 2",
+    value: "skylights",
+    longDescription:
+      "Tai yra specialios lango formos konstrukcijos, kurios įmontuojamos į stogą ir leidžia natūraliam šviesos srautui patenkinti į patalpas iš viršaus. Šios šviesos elementai gali būti statomos iš stiklo arba plastiko ir paprastai yra dizainuotos taip, kad būtų atsparios vandeniui.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Audra/lietus (palydovinis įrenginys)",
+    group: "value 3",
+    value: "storm/rain-satellite-unit",
+    longDescription:
+      "Gali būti vertinama kaip speciali prietaiso dalis, kuri yra integruota į smart home sistemą ir skirta stebėti oro sąlygas, ypač susijusias su audromis ir lietumi. Ši prietaiso dalis gali būti susijusi su meteorologinėmis stebėsenos sistemomis arba gauti informaciją iš meteorologinių palydovų.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Audra/lietus (universalus siųstuvas)",
+    group: "value 3",
+    value: "storm/rain-universal-transmitter",
+    longDescription:
+      "Gali būti vertinama kaip įrenginys, skirtas detaliai stebėti oro sąlygas, ypač susijusias su audromis ir lietumi, ir siųsti šią informaciją į smart home sistemą. Šis universalus perdaviklis gali būti susijęs su įvairiomis meteorologinėmis stebėsenos sistemomis arba palydovinėmis tarnybomis, kurios teikia informaciją apie orus.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Nuotėkis",
+    group: "value 3",
+    value: "leakage",
+    longDescription:
+      "Smart home sistemoje gali būti vertinama kaip funkcija, skirta aptikti vandens nutekėjimus arba nuotėkį namų viduje ir nedelsiant pranešti apie tai gyventojams. Ši funkcija gali būti įgyvendinta naudojant specialius vandens jutiklius arba sensorius, kurie yra įdiegti tam tikruose strategiškai svarbiuose vietose, tokiuose kaip virtuvės, vonios kambariai, skalbimo mašinos arba kiti vandens naudojimo taškai.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Kondensatas",
+    group: "value 3",
+    value: "condensation",
+    longDescription:
+      "Smart home sistemoje gali būti suprantama kaip funkcija, skirta aptikti kondensacijos atsiradimą ir valdyti tai susijusius veiksmus. Kondensacija yra procesas, kai oro drėgmė prisikerta ant šalto paviršiaus, pavyzdžiui, langų ar veidrodžių, ir virsta vandens lašais arba rūkeliu. Šis reiškinys gali sukelti ne tik estetinę problemą, bet ir potencialią pavojų namo interjero kokybei ir struktūros pažeidimą.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "CO2",
+    group: "value 3",
+    value: "CO2 ",
+    longDescription:
+      "Smart home sistemoje gali būti suprantama kaip funkcija, skirta aptikti anglies dioksido lygį patalpose ir įvairiais būdais valdyti oro kokybę.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Radiatoriaus",
+    group: "value 4",
+    value: "radiator-thermostat",
+    longDescription:
+      "Smart home sistemoje yra funkcija, kuri leidžia valdyti šildymo sistemos radiatorius naudojant išmaniuosius termostatus. Šis termostatas yra įdiegtas ant radiatoriaus ir gali būti valdomas išmaniajame telefone arba kitaip integruotoje sistemoje.Pagrindinė radiatoriaus termostato funkcija yra reguliuoti kambario temperatūrą, valdydama radiatoriaus šilumos išmetimą. Tai leidžia gyventojams individualiai nustatyti kiekvieno kambario temperatūrą ir taupyti energiją, nes galima nustatyti optimalų šilumos lygį kiekvienam kambaryje atsižvelgiant į jo naudojimąsi ir asmeninius poreikius.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Sieninis termostatas",
+    group: "value 4",
+    value: "wall-thermostat",
+    longDescription:
+      "tai yra įrenginys, kuris montuojamas ant sienos ir naudojamas valdyti kambario temperatūrą. Tai gali būti svarbi funkcija smart home sistemoje.Pagrindinė sieninio termostato funkcija yra reguliuoti šildymo arba vėdinimo sistemą, kad būtų išlaikytas pageidaujamas kambario temperatūros lygis. Įrenginys gali būti valdomas rankiniu būdu, naudojant jo fizinį ekraną ir mygtukus, arba automatiškai, pagal nustatytus grafikus arba scenarijus.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Boileris",
+    group: "value 4",
+    value: "boiler",
+    longDescription:
+      "Tai yra svarbus prietaisas šildymo sistemoje, kuris naudojamas šildyti vandenį. Tai svarbi funkcija smart home sistemoje, nes leidžia valdyti šildymo sistemą ir vandens šildymą, kad būtų optimizuotas energijos naudojimas ir patogumas.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Grindinis šildymas",
+    group: "value 4",
+    value: "underfloor-heating",
+    longDescription:
+      "Tai yra šildymo sistema, kurioje šiluma yra paskirstoma per grindis, o ne per tradicinius radiatorius ar oro srautus. Tai svarbi funkcija smart home sistemoje, nes leidžia valdyti kambario temperatūrą efektyviai ir komfortiškai.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Ventiliacija",
+    group: "value 4",
+    value: "ventilation",
+    longDescription:
+      "Tai yra svarbi funkcija smart home sistemoje, skirta valdyti oro cirkuliaciją ir kokybę patalpose.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Scenos per sieninį perdavėją",
+    group: "value 5",
+    value: "scenes-via-wall-transmitter",
+    longDescription:
+      "Tai reiškia, kad per sieninį perdavėją (pultelį) galima valdyti skirtingus scenarijus arba nustatymus smart home sistemoje. Scenos gali apimti įvairius veiksmus, pvz., reguliuoti apšvietimą, temperatūrą, uždaryti ar atidaryti žaliuzes ir kt. Sieninis perdavėjas suteikia patogumą ir greitą prieigą prie įvairių namo funkcijų, leisdamas greitai prisitaikyti prie gyvenimo būdo ar pageidavimų.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Scenos per programėlę ",
+    group: "value 5",
+    value: "scenes-via-app",
+    longDescription:
+      "Tai reiškia, kad per mobiliąją aplikaciją galima valdyti skirtingus scenarijus arba nustatymus smart home sistemoje. Scenos gali apimti įvairius veiksmus, pvz., įjungti ar išjungti apšvietimą, reguliuoti temperatūrą, valdyti saugumo sistemas ir kt. Mobili aplikacija suteikia patogumą ir galimybę valdyti namo funkcijas iš bet kurios vietos, kur yra interneto ryšys.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Scenos per balsų valdymą",
+    group: "value 5",
+    value: "scenes-via-voice-control",
+    longDescription:
+      "Tai reiškia, kad galima valdyti skirtingus scenarijus ar nustatymus smart home sistemoje naudojant balso komandas arba balsų asistentus, tokius kaip 'Alexa' ar 'Google Assistant'. Ši funkcija leidžia vartotojams aktyvuoti norimus scenarijus ar nustatymus, tiesiog kalbant į įrenginį, kuris turi integruotą balso valdymo technologiją.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Iki 34 scenų",
+    group: "value 5",
+    value: "up-to-34-scenes",
+    longDescription:
+      "Tai reiškia, kad yra galimybė sukurti arba programuoti iki 34 skirtingus scenarijus arba nustatymus smart home sistemoje. Šios scenos gali apimti įvairius veiksmus, tokius kaip įjungti/arba išjungti apšvietimą, reguliuoti temperatūrą, valdyti saugumo sistemas ir pan. Tai suteikia vartotojams didelį lankstumą ir galimybes pritaikyti savo namų automatizavimą pagal asmeninius poreikius ir gyvenimo būdą.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Iki 100 scenų",
+    group: "value 5",
+    value: "up-to-100-scenes",
+    longDescription:
+      "Tai reiškia, kad yra galimybė sukurti arba programuoti daugiau nei 100 skirtingų scenarijų ar nustatymų smart home sistemoje. Šios scenos gali apimti įvairius veiksmus, pavyzdžiui, įjungti ar išjungti apšvietimą, reguliuoti temperatūrą, valdyti saugumo sistemas ir t.t. Tai suteikia vartotojams labai didelį lankstumą ir galimybes pritaikyti savo namų automatizavimą pagal asmeninius poreikius ir gyvenimo būdą.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Neribotas scenų kiekis",
+    group: "value 5",
+    value: "unlimited-scenes",
+    longDescription:
+      "Tai reiškia, kad nėra jokio apribojimo, kiek scenų galima sukurti arba programuoti smart namų sistemoje. Vartotojai gali kurti tiek scenų, kiek nori, ir pritaikyti jas pagal savo individualius poreikius bei gyvenimo būdą. Tai suteikia didelį lankstumą ir galimybes maksimaliai pritaikyti automatizavimą pagal jų norus ir aplinkybes.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Energijos matavimas",
+    group: "value 6",
+    value: "energy-measurement",
+    longDescription:
+      "Tai yra funkcija, kuri leidžia matuoti, stebėti ir įvertinti elektros energijos naudojimą namuose arba įrenginyje. Ši funkcija gali būti integruota į smart home sistemą ir suteikti vartotojui informaciją apie tai, kiek energijos naudojama, kada ji naudojama ir kaip ji gali būti efektyviau valdoma. Tai padeda vartotojams sekti jų energijos sąnaudas, identifikuoti taupymo galimybes ir padėti priimti informuotus sprendimus, siekiant sumažinti energijos sąnaudas ir išlaikyti efektyvų energijos naudojimą.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "SENEC namų baterijos saugojimas",
+    group: "value 6",
+    value: "senec-home-battery-transmitter",
+    longDescription:
+      "Tai yra sistema, kuri leidžia saugoti perteklinę elektros energiją, pagamintą iš saulės ar kitų energijos šaltinių, naudojant baterijas. Ši sistema leidžia vartotojams naudoti saugotą energiją, kai jos reikia, pavyzdžiui, vakare ar rūsyje, kai saulė nenusileidžia arba elektros energijos poreikis yra didesnis. Tai padeda sumažinti elektros sąnaudas ir padidina savarankiškumą, naudojant alternatyvius energijos šaltinius.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Oro prognozė",
+    group: "value 7",
+    value: "weather-forecast",
+    longDescription:
+      "Tai yra informacija, kuri teikia numatytą orų būklę tam tikrame regione ateinančiomis dienomis ar valandomis. Ši informacija gali apimti temperatūrą, drėgmę, vėjo greitį ir kryptį, kritulių tikimybę ir kitus oro sąlygų rodiklius. Oro prognozės yra svarbios planuojant veiklą lauke arba pasiruošiant tam tikriems orų sąlygoms. Daugelis smart home sistemų gali įtraukti oro prognozės funkciją, kad vartotojams būtų lengviau planuoti jų dienas ir reguliuoti namų komfortą pagal numatytas orų sąlygas.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Oro stotelė",
+    group: "value 7",
+    value: "weather-station",
+    longDescription:
+      "Tai yra įrenginys, skirtas matuoti ir stebėti įvairius orų parametrus, tokiais kaip temperatūra, drėgmė, vėjo greitis ir kryptis, kritulių kiekis, slėgis ir kt. Oro stotelės gali būti naudojamos tiek asmeniniams, tiek komerciniams tikslams, teikianti išsamesnę ir tikslią informaciją apie orų sąlygas tam tikrame regione. Šios stotelės gali būti įdiegtos namų aplinkoje arba įvairiose vietose, pavyzdžiui, miestų centrų stoguose, oro uostuose, laivybose ar netgi kosminėse misijose. Taip pat įvairios smart home sistemos gali naudoti duomenis iš oro stotelės, kad būtų suteikta išsamesnė ir asmeniškesnė informacija apie orų sąlygas ir pagal tai prisitaikyti automatizuoti namų funkcijas.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Taisyklių kūrimas audrų atveju",
+    group: "value 7",
+    value: "creation-of-rules-for-storms",
+    longDescription:
+      "Tai reiškia sukurti nustatytas instrukcijas ar veiksmus, kurie bus vykdomi, kai įvyksta audros ar stiprūs atmosferos sąlygų pokyčiai.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Langų jutikliai",
+    group: "value 8",
+    value: "window-contacts",
+    longDescription:
+      "Tai yra įrenginiai, kurie yra sumontuoti ant langų ar durų ir skirti aptikti jų atidarymą ar uždarymą. Šie jutikliai gali būti dalis saugumo sistemos ir leidžia vartotojams stebėti ir valdyti namų saugumą. Kai langai ar durys yra atidaromi, jutikliai gali išsiųsti signalą į centrinę kontrolės sistemą, kuri gali inicijuoti įvairius veiksmus, pavyzdžiui, įjungti apsaugos signalą arba pranešti vartotojams apie galimą pavojų. Tai padeda užtikrinti namų saugumą ir apsaugą nuo nepageidaujamų įsilaužimų arba nelaimingų atsitikimų.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Durų jutikliai",
+    group: "value 8",
+    value: "door-contacts",
+    longDescription:
+      "Tai  yra įrenginiai, kurie montuojami ant durų ir skirti aptikti jų atidarymą ar uždarymą. Šie jutikliai yra svarbus komponentas saugumo sistemose ir leidžia vartotojams stebėti ir kontroliuoti durų būseną.Kai durys yra atidarytos ar uždarytos, jutikliai gali nustatyti šią būseną ir siųsti signalą į centralinę valdymo sistemą. Tai leidžia sistemos vartotojams gauti pranešimus apie durų būseną ir atlikti tam tikrus veiksmus, pavyzdžiui, įjungti arba išjungti saugumo funkcijas, registruoti durų naudojimo laiką arba prireikus pranešti apie nepageidaujamus įvykius, tokius kaip įsilaužimai.",
+    shortDescription: "Short description 1",
+  },
+  {
+    name: "Mirtino spynos jungiklio kontaktai",
+    group: "value 8",
+    value: "dead-bolt-switch-contacts",
+    longDescription:
+      "Tai yra kontaktai, kurie yra susiję su mirtinės spynos mechanizmu. Mirtinoji spyna yra mechanizmas, kuris yra sumontuotas ant durų ir suteikia papildomą saugumą užrakindamas duris iš vidaus. Kai spyna yra įsijungiama arba išjungiama, šie kontaktai aptinka šią būseną ir gali siųsti signalą į centrinius valdymo sistemą.",
     shortDescription: "Short description 1",
   },
 ];
