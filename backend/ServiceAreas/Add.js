c8130cc630ebadf758a43142d90653c7ac5235fa
viktoras8 <viktoraseikis@gmail.com>
2024-03-24 22:47:08 +0200

    pabaigiau parasyti visus duomenis i duomenu baze

--- backend/ServiceAreas/Add.js
+++ backend/ServiceAreas/Add.js
@@ -1,68 +1,107 @@
 export const serviceAreasToAdd = [
-    {
-      name: 'Apšvietimas',
-      group: 'value 1',
-      longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      shortDescription: 'Short description 2',
-    },
-    {
-      name: 'Uždangos',
-      group: 'value 2',
-      longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      shortDescription: 'Short description 2',
-    },
-    {
-      name: 'Apsauga',
-      group: 'value 3',
-      longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      shortDescription: 'Short description 2',
-    },
-    {
-      name: 'Service Area 4',
-      group: 'value 4',
-      longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      shortDescription: 'Short description 2',
-    },
-    {
-      name: 'Service Area 5',
-      group: 'value 5',
-      longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      shortDescription: 'Short description 2',
-    },
-    {
-      name: 'Service Area 6',
-      group: 'value 6',
-      longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      shortDescription: 'Short description 2',
-    },
-    {
-      name: 'Service Area 7',
-      group: 'value 7',
-      longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      shortDescription: 'Short description 2',
-    },
-    {
-      name: 'Service Area 8',
-      group: 'value 8',
-      longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      shortDescription: 'Short description 2',
-    },
-    {
-      name: 'Service Area 9',
-      group: 'value 9',
-      longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      shortDescription: 'Short description 2',
-    },
-    {
-      name: 'Service Area 10',
-      group: 'value 10',
-      longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      shortDescription: 'Short description 2',
-    },
-    {
-      name: 'Service Area 11',
-      group: 'value 11',
-      longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      shortDescription: 'Short description 2',
-    },
+  {
+    name: "Apšvietimas",
+    group: "apsvietimas",
+    longDescription:
+      "Apsvietimas išmaniuose namuose apima naudojimąsi technologijomis, skirtomis valdyti ir reguliuoti apšvietimą namuose naudojant išmanius įrenginius. Tai gali būti įvairaus pobūdžio sprendimai, kurie leidžia valdyti apšvietimą per išmaniuosius telefonus, planšetinius kompiuterius arba kitus prietaisus, dažniausiai naudojant specialias programėles arba nuotolinio valdymo įrenginius.",
+    shortDescription: "Short description 2",
+  },
+  {
+    name: "Uždangos",
+    group: "uzdangos",
+    longDescription:
+      "Šviesos apšvietime gali būti naudojamos kaip funkcija, leidžianti kontroliuoti šviesos kryptį arba intensyvumą. Šviesos žaliuzės arba lankstūs šviesos elementai gali būti reguliuojami, kad būtų pasiekta norima šviesos skleidimo kryptis arba plotas. ",
+    shortDescription: "Short description 2",
+  },
+  {
+    name: "Apsauga",
+    group: "apsauga",
+    longDescription:
+      "Smart namų sistemoje yra svarbi funkcija, skirta užtikrinti gyventojų ir jų turtų apsaugą. Ši funkcija apima įvairius įrankius ir technologijas, kurios leidžia atpažinti ir reaguoti į bet kokius potencialius pavojus ar nesklandumus.",
+    shortDescription: "Short description 2",
+  },
+  {
+    name: "Šildymas",
+    group: "sildymas",
+    longDescription:
+      "Tai  yra pagrindinė funkcija smart home sistemoje, skirta valdyti vidaus klimato sąlygas ir užtikrinti patogumą bei energijos efektyvumą.",
+    shortDescription: "Short description 2",
+  },
+  {
+    name: "Scenos",
+    group: "scenes",
+    longDescription:
+      "Scenos išmaniuose namuose yra nustatytos apšvietimo, garso, temperatūros ir kitos namo srities parametrų kombinacijos, kurios gali būti lengvai aktyvuojamos vienu paspaudimu arba automatiškai pagal tam tikrus nustatytus scenarijus.",
+    shortDescription: "Short description 2",
+  },
+  {
+    name: "Energija",
+    group: "energija",
+    longDescription:
+      "Išmanių namų energijos valdymo sistemos suteikia vartotojams galimybę efektyviai naudoti energiją ir taupyti sąnaudas. Naudojant išmanius skaitiklius, jutiklius ir automatizavimo technologijas, namo įrenginiai gali būti reguliuojami pagal realaus laiko duomenis, nustatytus grafikus arba netgi sąlygas, pavyzdžiui, buvimo namuose arba laiko. Automatinis energijos valdymas ir taupymo patarimai leidžia optimizuoti vartojimą, o atsinaujinančių energijos šaltinių integracija padeda sumažinti priklausomybę nuo tradicinių, neatsinaujinančių energijos šaltinių. Visa tai kartu leidžia vartotojams ne tik sutaupyti pinigų, bet ir prisidėti prie tvaraus gyvenimo būdo ir aplinkos apsaugos.",
+    shortDescription: "Short description 2",
+  },
+  {
+    name: "Oras",
+    group: "oras",
+    longDescription:
+      "Oras yra kritiškas veiksnys, kurį galima kontroliuoti išmaniajame name, siekiant užtikrinti patogią ir sveiką gyvenamąją aplinką. Šiuolaikinės technologijos, tokiomis kaip išmanieji termostatai, klimato kontrolės sistemos ir ventiliacijos įrenginiai, leidžia reguliuoti temperatūrą, drėgmę ir oro cirkuliaciją, kad būtų pasiektas optimalus oras. Integruotos oro valymo sistemos gali filtruoti kenksmingas daleles ir cheminės medžiagas, pagerindamos oro kokybę ir sumažindamos alergijų pavojų. Be to, išmanieji namai gali suteikti pranešimus apie orą, leisdami gyventojams būti informuotiems apie svarbius orų pokyčius. Visa tai padeda kurti sveiką, komfortišką ir saugią gyvenamąją aplinką, kuri prisideda prie bendros gerovės ir patogumo išmaniajame name.",
+    shortDescription: "Short description 2",
+  },
+  {
+    name: "Langų ir durų stebėjimas",
+    group: "langu-ir-duru-stebejimas",
+    longDescription:
+      "Langų ir durų stebėjimas išmaniajame name suteikia galimybę vartotojams kontroliuoti namo saugumą ir patogumą. Naudojant įrenginius su jutikliais, galima stebėti, ar langai ir durys yra atidaryti ar uždaryti, bei gauti pranešimus apie bet kokią neįprastą veiklą. Tai padeda užtikrinti saugų ir patogų namų aplinką.",
+    shortDescription: "Short description 2",
+  },
+  {
+    name: "Vertės rodymas",
+    group: "vertes-rodymas",
+    longDescription:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    shortDescription: "Short description 2",
+  },
+  {
+    name: "Laikmačio jungikliai",
+    group: "laikmacio-jungikliai",
+    longDescription:
+      "Laikmačio jungikliai yra įrenginiai, kurie leidžia nustatyti konkretų laiką arba laiko intervalą, kada įrenginys arba sistema turi būti įjungta arba išjungta. Tai naudinga funkcija išmaniame name, leidžianti automatizuoti įvairius procesus, pavyzdžiui, apšvietimą arba šildymo sistemą, pagal nustatytus laiko grafikus. Taip vartotojai gali taupyti energiją ir užtikrinti efektyvesnį įrenginių naudojimą, nebereikalingai neįjungiant arba neapšviestu namo tuomet, kai to nereikia.",
+    shortDescription: "Short description 2",
+  },
+  {
+    name: "Galutinio vartotojo konfigūravimo galimybės",
+    group: "galutinio-vartotojo-konfiguravimo-galimybes",
+    longDescription:
+      "Galutinio vartotojo konfigūravimo galimybės išmaniajame name suteikia vartotojams kontrolę ir pritaikomumą pagal jų poreikius. Tai apima scenų kūrimą, nuotolinį valdymą, parametrų pritaikymą, pranešimų valdymą bei apsaugos ir privatumo užtikrinimą. Tai leidžia vartotojams optimaliai pasinaudoti išmanaus namo technologija.",
+    shortDescription: "Short description 2",
+  },
+  {
+    name: "Vartotojo valdymas",
+    group: "vartotojo-valdymas",
+    longDescription:
+      "Vartotojo valdymas išmaniajame name leidžia valdyti funkcijas per programėles, nuotolinį valdymą, balsinį valdymą ir individualius profilius, užtikrinant asmenišką ir patogų namų valdymą.",
+    shortDescription: "Short description 2",
+  },
+  {
+    name: "Buvimo modeliavimas",
+    group: "buvimo-modeliavimas",
+    longDescription:
+      "Buvimo modeliavimas išmaniajame name naudoja jutiklius ir algoritmus, kad nustatytų, ar namuose yra gyventojai. Tai leidžia automatiškai reguliuoti šildymą, apšvietimą ir kitas funkcijas pagal buvimo vietą, taupant energiją ir užtikrinant patogumą.",
+    shortDescription: "Short description 2",
+  },
+  {
+    name: "Loginės funkcijos",
+    group: "logines-funkcijos",
+    longDescription:
+      "Logines funkcijos išmaniajame name tai programinės logikos konstrukcijos, valdančios namo įrenginius. Jos nustato sąlygas, pagal kurias įrenginiai veikia arba ne. Tai svarbus komponentas automatizuotuose namuose, užtikrinant efektyvų energijos naudojimą ir funkcionalumą.",
+    shortDescription: "Short description 2",
+  },
+  {
+    name: "Integracija kitų sistemų",
+    group: "integracija-kitu-sistemu",
+    longDescription:
+      "Logines funkcijos išmaniajame name tai programinės logikos konstrukcijos, valdančios namo įrenginius. Jos nustato sąlygas, pagal kurias įrenginiai veikia arba ne. Tai svarbus komponentas automatizuotuose namuose, užtikrinant efektyvų energijos naudojimą ir funkcionalumą.",
+    shortDescription: "Short description 2",
+  },
 ];
