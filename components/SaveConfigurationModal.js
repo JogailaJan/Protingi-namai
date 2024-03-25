57542a7bba18bf3486a0f2e90039135a7a6def8c
JogailaJan <jogaila.janusas@gmail.com>
2024-03-26 00:21:04 +0200

    Revert "Revert "Nebezinau ka bedaryt, reiks revertint ir kazkaip keist""
    This reverts commit 2172a5a509f42363746b1183f0ab2754903a906a.

--- components/SaveConfigurationModal.js
+++ components/SaveConfigurationModal.js
@@ -1,36 +1,82 @@
-import React, { useState } from 'react';
-import { Modal, View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 
-const SaveConfigurationModal = ({ visible, onSave, onCancel }) => {
+const SaveConfigurationModal = ({visible, onDismiss}) => {
   const [configurationName, setConfigurationName] = useState('');
 
-  const handleSave = () => {
-    onSave(configurationName);
-    setConfigurationName('');
+  const handleSave = async (name) => {
+    // if (selectedConfiguration) {
+    //   const updatedConfigurations = savedConfigurations.map((config) => {
+    //     if (config.name === selectedConfiguration.name) {
+    //       return { name: selectedConfiguration.name, items: selectedItems };
+    //     }
+    //     return config;
+    //   });
+    //   setSavedConfigurations(updatedConfigurations);
+    //   await AsyncStorage.setItem(
+    //     STORAGE_KEY,
+    //     JSON.stringify(updatedConfigurations)
+    //   );
+    // } else {
+    //   saveConfiguration(name);
+    // }
+    // onDismiss();
+    // setConfigurationName("");
+  };
+
+  const saveConfiguration = async (name) => {
+    // try {
+    //   console.log("Saving configuration with name:", name);
+    //   console.log("Items:", selectedItems);
+    //   const updatedConfigurations = [
+    //     ...savedConfigurations,
+    //     { name: name, items: selectedItems },
+    //   ];
+    //   await AsyncStorage.setItem(
+    //     STORAGE_KEY,
+    //     JSON.stringify(updatedConfigurations)
+    //   );
+    //   setSavedConfigurations(updatedConfigurations);
+    //   console.log(
+    //     "Number of configurations saved:",
+    //     updatedConfigurations.length
+    //   );
+    // } catch (error) {
+    //   console.error("Error saving configuration:", error);
+    // }
+  };
+
+  const handleCancel = () => {
+    onDismiss();
+    //setIsSaveModalVisible(false);
+    console.log("Atsaukiama, todel slepiama")
+    setConfigurationName("");
   };
 
   return (
-    <Modal
-      visible={visible}
-      transparent={true}
-      animationType="slide"
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={[
+        styles.modalContainer,
+        {
+          display: visible ? 'flex' : 'none',
+        },
+      ]}
     >
-      <View style={styles.modalContainer}>
-        <View style={styles.modalContent}>
-          <Text style={styles.label}>Konfigūracijos pavadinimas:</Text>
-          <TextInput
-            style={styles.input}
-            value={configurationName}
-            onChangeText={text => setConfigurationName(text)}
-            placeholder="Suteikite konfigūracijai pavadinimą"
-          />
-          <View style={styles.buttonsContainer}>
-            <Button title="Atšaukti" onPress={onCancel} />
-            <Button title="Išsaugoti" onPress={handleSave} />
-          </View>
+      <View style={styles.modalContent}>
+        <Text style={styles.label}>Konfigūracijos pavadinimas:</Text>
+        <TextInput
+          style={styles.input}
+          value={configurationName}
+          onChangeText={text => setConfigurationName(text)}
+          placeholder="Suteikite konfigūracijai pavadinimą"
+        />
+        <View style={styles.buttonsContainer}>
+          <Button title="Atšaukti" onPress={handleCancel} />
+          <Button title="Išsaugoti" onPress={handleSave} />
         </View>
       </View>
-    </Modal>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -40,12 +86,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    zIndex: -1,
   },
   modalContent: {
     backgroundColor: 'white',
     padding: 20,
     borderRadius: 10,
-    elevation: 5,
+    elevation: 1,
   },
   label: {
     marginBottom: 5,
@@ -57,6 +104,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 10,
+    width: '100%', // Ensure TextInput takes full width
   },
   buttonsContainer: {
     flexDirection: 'row',
